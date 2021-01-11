e9f6f53f2a650d2adefbfc61bf755156dace4665
Dj Man <davidmesak@hotmail.com>
2021-01-11 16:25:43 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/dj/DJ/Senior College/Distributed Systems/Assignments/Assignment 4/Program
+# Build directory: /media/dj/DJ/Senior College/Distributed Systems/Assignments/Assignment 4/Program/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
